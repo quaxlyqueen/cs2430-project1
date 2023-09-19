b7ca55749eca78b715787764391a49a5d3eb3dbf
LexusLindeman <135858125+LexusLindeman@users.noreply.github.com>
2023-09-18 20:42:42 -0600

    Update ShakerSort.java

--- src/algorithms/ShakerSort.java
+++ src/algorithms/ShakerSort.java
@@ -5,8 +5,29 @@ public class ShakerSort {
      * @param int[] arr, an array containing a permutation.
      * @return the number of comparisons
      */
-    public static int shakerSort(int[] arr) {
-        return -1; // TODO: @Lexus :3
+   public static void shakerSort(int[] array) {
+            
+		for (int i = 0; i < array.length / 2; i++) {
+			boolean swapped = false;
+			for (int j = i; j < array.length - i - 1; j++) {
+				if (array[j] > array[j + 1]) {
+					int tmp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = tmp;
+					swapped = true;
+				}
+			}
+			for (int j = array.length - 2 - i; j > i; j--) {
+				if (array[j] < array[j - 1]) {
+					int tmp = array[j];
+					array[j] = array[j - 1];
+					array[j - 1] = tmp;
+					swapped = true;
+				}
+			}
+			if (!swapped)
+				break;
+        }
     }
 
 }
